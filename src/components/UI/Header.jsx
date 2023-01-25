86ac260d444720ae83bb55e4f3aa47d8338a1a88
IgnasPlace <ignasplace@gmail.com>
2023-01-25 11:38:10 +0100

    changed favorite colors container min-height

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -4,16 +4,13 @@ import { useState } from "react";
 import { IoIosMenu, IoIosArrowBack } from "react-icons/io";
 import FavoriteColorCard from './FavoriteColorCard';
 
-const Header = () => {
+const Header = ({windowHeightMinusHeader}) => {
   const [store, dispatch] = useColors();
   const [favoritesVisible, setFavoritesVisible] = useState(false);
 
   const favoritesClasses = favoritesVisible
     ? `${classes.favorites} ${classes.visible}`
     : `${classes.favorites}`;
-
-  const overlayClasses = favoritesVisible ? `${classes.overlay} ${classes.overlayOpen}`
-  : `${classes.overlay}`;
 
   const toggleMenu = () => {
     setFavoritesVisible((prev) => !prev);
@@ -30,7 +27,7 @@ const Header = () => {
           </menu>
           {favoritesVisible ? <h3 className={classes.favoriteColorsText}>Favorite Colors</h3> : null}
         </nav>
-        <section className={favoritesClasses}>
+        <section className={favoritesClasses} style={{minHeight: windowHeightMinusHeader}}>
           {store.favoriteColors.map(favColor => {
             return <FavoriteColorCard key={favColor} favColor={favColor} />
           })}
