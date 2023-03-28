104074f9ee81e8f2e5d53668fa5e3117704ec1b5
IgnasPlace <ignasplace@gmail.com>
2023-03-28 17:23:48 +0200

    added a paragraph in case there are no favorite colors

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -2,9 +2,9 @@ import classes from "./Header.module.css";
 import { useColors } from "../../store/color-context";
 import { useState } from "react";
 import { IoIosMenu, IoIosArrowBack } from "react-icons/io";
-import FavoriteColorCard from './FavoriteColorCard';
+import FavoriteColorCard from "./FavoriteColorCard";
 
-const Header = ({windowHeightMinusHeader}) => {
+const Header = ({ windowHeightMinusHeader }) => {
   const [store, dispatch] = useColors();
   const [favoritesVisible, setFavoritesVisible] = useState(false);
 
@@ -20,17 +20,36 @@ const Header = ({windowHeightMinusHeader}) => {
     <>
       <header className={classes.header}>
         <nav className={classes.nav}>
-          <menu className={classes.menu} >
+          <menu className={classes.menu}>
             <div onClick={toggleMenu}>
               {favoritesVisible ? <IoIosArrowBack /> : <IoIosMenu />}
             </div>
           </menu>
-          {favoritesVisible ? <h3 className={classes.favoriteColorsText}>Favorite Colors</h3> : <h3 className={classes.favoriteColorsText}>Color Generator</h3>}
+          {favoritesVisible ? (
+            <h3 className={classes.favoriteColorsText}>Favorite Colors</h3>
+          ) : (
+            <h3 className={classes.favoriteColorsText}>Color Generator</h3>
+          )}
         </nav>
-        <section className={favoritesClasses} style={{minHeight: windowHeightMinusHeader}}>
-          {store.favoriteColors.map(favColor => {
-            return <FavoriteColorCard key={favColor} favColor={favColor} />
-          })}
+        <section
+          className={favoritesClasses}
+          style={{ minHeight: windowHeightMinusHeader }}
+        >
+          {store.favoriteColors.length > 0 ? (
+            store.favoriteColors.map((favColor) => {
+              return <FavoriteColorCard key={favColor} favColor={favColor} />;
+            })
+          ) : (
+            <p className={classes.noFavoriteColors}>
+              No favorite colors found.
+              <br />
+              <br />
+              <span>
+                Please click the Heart icon if you wish to save the color for
+                later.
+              </span>
+            </p>
+          )}
         </section>
       </header>
     </>
